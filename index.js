85bfdc157b0fc3bff7bffdec9a600816180db637
Juan Pablo Garcia Martinez <juanpablogm3@hotmail.com>
2022-11-14 23:00:10 -0300

    ejercicios del curso de Javascript

---
+++ index.js
@@ -0,0 +1,16 @@
+let numeroIngresado = Number(prompt('dame un numero grande: '));
+if(numeroIngresado > 1000){
+    alert('Conseguiste que sea mayor a mil, felicidades')
+} else {
+    alert ('Fallaste, prueba con 10 x 100')
+}
+
+let txt = prompt('Ingrese un texto: ');
+if(txt=='Hola'){
+    alert('Dijiste Hola')
+}
+
+let num = Number(prompt('Ingrese num: '))
+if((num>10)&&(num<50)){
+    alert('El numero es entre 10 y 50');
+}
